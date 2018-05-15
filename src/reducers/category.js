607716b36a2a288fc3b4d9f05ed7618c997b4506
Renoll, Lynn Ashley <lrenoll@gatech.edu>
2018-05-15 18:17:02 -0400

    Updating categories reducer
    Shortening syntax by moving switch with 1 condition to that of an if statement.

--- src/reducers/category.js
+++ src/reducers/category.js
@@ -1,14 +1,12 @@
 //Category Reducer, dispatch an action here.  
 
 function category(state = {}, action){
-  switch (action.type) {
-    case 'GET_ALL_CATEGORIES':
-      return {
-        ...state,
-        categories: action.categories
-      };
-    default:
-      return state;
+  if (action.type === 'GET_ALL_CATEGORIES'){
+    return {
+      ...state,
+      categories: action.categories
+    };
   }
+  return state;
 }
 export default category;
